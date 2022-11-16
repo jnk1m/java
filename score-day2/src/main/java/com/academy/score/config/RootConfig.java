c78033aab9c4da181ef08c33b8e994e8dc5b0129
jnkm <103839217+jnk1m@users.noreply.github.com>
2022-11-16 13:50:17 +0900

    Update RootConfig.java
    RootConfig bean userRepo and studentRepo has been fixed

--- score-day2/src/main/java/com/academy/score/config/RootConfig.java
+++ score-day2/src/main/java/com/academy/score/config/RootConfig.java
@@ -8,26 +8,30 @@ import com.academy.score.repository.UserRepositoryImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-        userRepository.register("admin@admin.com", "12345");
-
-                return userRepository;
-                }
-
-@Bean
-public StudentRepository studentRepository() {
 import org.springframework.stereotype.Controller;
+
+
+
+
 
 @Configuration
 @ComponentScan(basePackageClasses = Base.class,
         excludeFilters = {@ComponentScan.Filter(Controller.class)})
 public class RootConfig {
     @Bean
-    public UserRepository userRepository() {
-        UserRepository userRepository = new UserRepositoryImpl();
+    public StudentRepository studentRepository() {
         StudentRepository studentRepository = new StudentRepositoryImpl();
         studentRepository.register("Dave Grohl","dave@gorhl.com",100,"American rock band the Foo Fighter's front man.");
 
         return studentRepository;
+
+    }
+    @Bean
+    public UserRepository userRepository() {
+        UserRepository userRepository = new UserRepositoryImpl();
+        userRepository.register("admin@admin.com", "12345");
+
+        return userRepository;
     }
 
 }
